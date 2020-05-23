6bda09eee317ebb30a85ae2304d2c540a901d9b6
Tyler Haskell <tyler.haskell@concur.com>
2020-05-23 13:23:25 -0700

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Haskell, Tyler on 5/23/20.
+//  Copyright © 2020 Tyler Haskell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
